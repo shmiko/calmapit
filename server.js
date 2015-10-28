ff2f6b51997878a6f35a079074845b690db7b0ac
Paul Jones <shmiko@gmail.com>
2015-10-28 23:27:25 +1100

    server port

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/auth', function(req, res) {
     } 
 });
 
-var server = app.listen(2002, function() {
+var server = app.listen(2222, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('************************');
